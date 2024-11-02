9fb7f4148a88eec0aa9302c240216f09a1f1662b
Dmitriy Kryzhanovskiy <dmitr.kryzh@gmail.com>
2024-11-02 23:50:59 +0300

    feat: implement custom role assigner authenticator and configure Keycloak with PostgreSQL and PgAdmin
    - Added `CustomRoleAssignerAuthenticator` and `CustomRoleAssignerAuthenticatorFactory` classes for assigning roles based on `userRole` attribute during authentication in Keycloak.
    - Configured Docker Compose with PostgreSQL and PgAdmin services for Keycloak database support.
    - Updated Dockerfile to build and include the custom authenticator SPI in Keycloak container.

---
+++ keycloak/keycloak-spi/src/main/java/org/champzone/CustomRoleAssignerAuthenticatorFactory.java
@@ -0,0 +1,93 @@
+package org.champzone;
+
+import org.keycloak.Config;
+import org.keycloak.authentication.Authenticator;
+import org.keycloak.authentication.AuthenticatorFactory;
+import org.keycloak.models.AuthenticationExecutionModel;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+import org.keycloak.provider.ProviderConfigProperty;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.keycloak.provider.ProviderConfigProperty.STRING_TYPE;
+
+public class CustomRoleAssignerAuthenticatorFactory implements AuthenticatorFactory {
+    public static final String PROVIDER_ID = "custom-role-assigner";
+    static final String CUSTOM_ROLE_ASSIGNER_CONFIG = "custom_role_assigner";
+    static final String HELP_TEXT = "Assigns roles to users based on the 'user_role' attribute ('client' or 'coach') specified during registration.";
+
+    @Override
+    public Authenticator create(KeycloakSession session) {
+        return new CustomRoleAssignerAuthenticator();
+    }
+
+    @Override
+    public void init(Config.Scope scope) {
+    }
+
+    @Override
+    public void postInit(KeycloakSessionFactory keycloakSessionFactory) {
+    }
+
+    @Override
+    public void close() {
+    }
+
+    @Override
+    public String getId() {
+        return PROVIDER_ID;
+    }
+
+    @Override
+    public String getDisplayType() {
+        return "Custom Role Assigner Authenticator";
+    }
+
+    @Override
+    public String getReferenceCategory() {
+        return "Role Assignment";
+    }
+
+    @Override
+    public boolean isConfigurable() {
+        return false;
+    }
+
+    @Override
+    public AuthenticationExecutionModel.Requirement[] getRequirementChoices() {
+        return new AuthenticationExecutionModel.Requirement[]{
+                AuthenticationExecutionModel.Requirement.REQUIRED,
+                AuthenticationExecutionModel.Requirement.ALTERNATIVE,
+                AuthenticationExecutionModel.Requirement.DISABLED
+        };
+    }
+
+    @Override
+    public boolean isUserSetupAllowed() {
+        return false;
+    }
+
+    @Override
+    public String getHelpText() {
+        return HELP_TEXT;
+    }
+
+
+    @Override
+    public List<ProviderConfigProperty> getConfigProperties() {
+        ProviderConfigProperty name = new ProviderConfigProperty();
+
+        name.setType(STRING_TYPE);
+        name.setName(CUSTOM_ROLE_ASSIGNER_CONFIG);
+        name.setLabel("HAHAHA");
+        name.setHelpText(HELP_TEXT);
+
+        return Collections.singletonList(name);
+    }
+//    @Override
+//    public List<ProviderConfigProperty> getConfigProperties() {
+//        return List.of();
+//    }
+}
